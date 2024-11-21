a3a8e3421d4c221254e358dfb83e550d12ec27c3
abrar-fahim-priom <priomshads@gmail.com>
2024-11-21 18:47:13 +0600

    fixed cors error

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ require("dotenv").config();
 const app = express();
 
 // Prevent CORS errors
-app.use(cors({ credentials: true, origin: true }));
+app.use(
+  cors({
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+    origin: ["http://localhost:5173", "http://localhost:8080"],
+  })
+);
 
 app.use(express.static(__dirname + "/public"));
 app.use("/uploads", express.static("uploads"));
@@ -27,29 +33,29 @@ app.use(jsonServer);
 // Error handle Middleware
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, _next) => {
-	res.status(500).json({
-		error: err.message,
-	});
+  res.status(500).json({
+    error: err.message,
+  });
 });
 
 // Not Found Middleware
 app.use((req, res) => {
-	res.status(404).json({
-		message: "Route Not Found",
-	});
+  res.status(404).json({
+    message: "Route Not Found",
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	connectDB()
-		.then(() => {
-			console.info(`Server running at port:${PORT}`);
-			console.log("http://localhost:3000");
-		})
-		.catch((err) => {
-			console.error(err);
-			throw new Error(err);
-		});
+  connectDB()
+    .then(() => {
+      console.info(`Server running at port:${PORT}`);
+      console.log("http://localhost:3000");
+    })
+    .catch((err) => {
+      console.error(err);
+      throw new Error(err);
+    });
 });
 
 module.exports = app;
